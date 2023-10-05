e2ae5225d005e6fe39fa592bdd045c171bae8d14
theLawrenzo <thelawrenzo1@gmail.com>
2023-10-05 11:46:28 -0700

    Update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Programt that prints to standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
+
+	return (1);
+}
