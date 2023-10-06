3f3791662ab7b558ba2c9fd65ade7177ebadbc1d
theLawrenzo <thelawrenzo1@gmail.com>
2023-10-05 21:56:15 -0700

    Update

---
+++ 0x00-hello_world/101-quotes.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * main - Program that writes to standared error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	return (write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1));
+}
