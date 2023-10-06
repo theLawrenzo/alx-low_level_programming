4aacaa5c73e7d287018c7b27e1500803970d4ca9
theLawrenzo <thelawrenzo1@gmail.com>
2023-10-05 21:38:06 -0700

    Update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Programt that prints to standard error
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
