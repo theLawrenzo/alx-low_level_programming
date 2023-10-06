3aee676a210798474a252377b939bf09697ba8da
theLawrenzo <thelawrenzo1@gmail.com>
2023-10-06 15:36:00 -0700

    Program that checks for the value of the last digit of a number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+#include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - Program that generates a random everytime it is
+ * executed
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	int n;
