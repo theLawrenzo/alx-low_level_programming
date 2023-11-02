a357b595c221b17c77708ed713a610076b244bb6
theLawrenzo <thelawrenzo1@gmail.com>
2023-11-01 21:52:37 -0700

    Program that prints change for the minimum number of conins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -26,6 +26,22 @@ int main(int argc, char *argv[])
 		printf("0\n");
 	}
 
+	while (num > 0)
+	{
+		if (num >= 25)
+			num -= 25;
+		else if (num == 10)
+			num -= 10;
+		else if (num == 5)
+			num -= 5;
+		else if (num == 2)
+			num -= 2;
+		else if (num == 1)
+			num -= 1;
+	}
+
+	printf("%d\n", num);
+
 
 
 	return (0);
