94483402002c07d47f98a398f4fd87fc10366441
theLawrenzo <thelawrenzo1@gmail.com>
2023-11-06 17:28:26 -0800

    A data structure of type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: An identifier for the dog
+ * @age: The age of the dog
+ * @owner: The dog owner's name
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif	/* DOG_H */
