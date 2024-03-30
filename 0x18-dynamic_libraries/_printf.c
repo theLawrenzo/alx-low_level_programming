cf6ba43aa3695dfe82228ae5abeb30f7b80513dc
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-03-29 21:27:03 -0700

    Inject me in and let me do some damage

--- 0x18-dynamic_libraries/_printf.c
+++ 0x18-dynamic_libraries/_printf.c
@@ -10,7 +10,7 @@
  */
 int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 9\n", 14);
+	write(1, "9 8 10 24 75 9\n", 17);
 	write(1, "Congratulations, you win the Jackot!\n", 38);
 	exit(EXIT_SUCCESS);
 }
