0c3a20ce06c1747221242c47c98ba823e487be4b
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-03-29 21:21:13 -0700

    The injection for the code injection task

---
+++ 0x18-dynamic_libraries/_printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * printf - Prints to file stream
+ * @format: Last operand befor ellipsis
+ *
+ * Return: 0 on success
+ */
+int printf(const char *format, ...)
+{
+	write(1, "9 8 10 24 75 9\n", 14);
+	write(1, "Congratulations, you win the Jackot!\n", 38);
+	exit(EXIT_SUCCESS);
+}
