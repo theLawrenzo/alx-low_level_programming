a5248f5393ec6265f6102a64fc69ab871ab3fcc6
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-09-11 21:05:43 -0700

    C SOURCE - Inserts a node in a DLL at a specified position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+ * dll_len - Returns the length of a dll
+ * @head: Pointer to the starting node
+ *
+ * Return: len
+ */
+size_t dll_len(dlistint_t *h)
+{
+	dlistint_t *tmp;
+	size_t len = 0;
+
+	if (h == NULL)
+		return (0);
+	tmp = h;
+	while (tmp != NULL)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+
+	return (len);
+}
+
+/**
  * insert_dnodeint_at_index - Inserts a node at a given position
  * @h: Pointer to the head of the list
  * @idx: Index to insert node
@@ -10,30 +33,28 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *tmp = *h;
-	int i = 0;
+	dlistint_t *new_node, *tmp = *h;
+	size_t len, i;
 
-	if (tmp == NULL || idx < 0)
+	len = dll_len(*h);
+	if (idx >= len)
 		return (NULL);
-
-	while (tmp != NULL)
+	if (idx == 0)
+		new_node = add_dnodeint(h, n);
+	else if (idx == (len - 1))
+		new_node = add_dnodeint_end(h, n);
+	else
 	{
-		if (idx == 0)
-		{
-			new_node = add_dnodeint(h, n);
-			break;
-		}
-		if (tmp->next == NULL)
-		{
-			new_node = add_dnodeint_end(h, n);
-			break;
-		}
+		for (i = 0; i < idx; i++)
+			tmp = tmp->next;
 		new_node = malloc(sizeof(dlistint_t));
 		if (new_node == NULL)
 			return (NULL);
 		new_node->next = tmp;
 		new_node->prev = tmp->prev;
+		new_node->n = n;
+		tmp->prev->next = new_node;
 		tmp->prev = new_node;
 	}
+	return (new_node);
 }
