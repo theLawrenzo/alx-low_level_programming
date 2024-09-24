e92e8d4f1168dfaed9cd4d608188dced7228213a
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-09-23 17:06:20 -0700

    Upadate

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	int i, len, sum, dec_num;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	len = strlen(b);
+	sum = 0;
+	dec_num = 1;
+
+	for (i = (len - 1); i >= 0; i--)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] == '1')
+			sum += dec_num;
+		else if (b[i] == '0')
+		{
+			dec_num *= 2;
+			continue;
+		}
+		else
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+
+		dec_num *= 2;
 	}
 
-	return (dec_val);
+	return (sum);
 }
